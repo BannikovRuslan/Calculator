f9901ae425736a31d2ae3b8953b37a21d1428a35
Ruslan <bannikov.ru@inbox.ru>
2021-06-08 23:15:33 +0500

    next

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,21 +1,18 @@
-
 import uvicorn
 from fastapi import FastAPI
 
 from backend.containers import Container
-from backend.sevices import RandomGenerator
 from backend.web import api
-from backend.web.api import app
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
     parameters = {"a": -10, "b": 10}
 
     container = Container()
     container.config.from_dict(parameters)
     container.wire(modules=[api])
 
-    app = FastAPI()
-    app.container = container
-    app.include_router(api.app)
+    app_container = FastAPI()
+    app_container.container = container
+    app_container.include_router(api.router)
+
+    uvicorn.run(app_container, host="0.0.0.0", port=8000)
--- app/backend/web/api.py
+++ app/backend/web/api.py
@@ -1,26 +1,26 @@
 from dependency_injector.wiring import Provide
 
 from backend.sevices.Calculator import Calculator
-from fastapi import FastAPI, Depends
+from fastapi import Depends, APIRouter
 from backend.containers import Container
 from backend.sevices.RandomGenerator import RandomGenerator
 
-app = FastAPI()
+router = APIRouter()
 
 
-@app.put("/calculate/plus")
+@router.put("/calculate/plus")
 def calculate_plus(x: float, y: float):
     calculator = Calculator()
     return {"x": x, "y": y, "result:": calculator.plus(x, y)}
 
 
-@app.put("/calculator/minus")
+@router.put("/calculator/minus")
 def calculate_minus(x: float, y: float):
     calculator = Calculator()
     return {"x": x, "y": y, "result:": calculator.minus(x, y)}
 
 
-@app.put("/random/range")
+@router.put("/random/range")
 def random_range(n: int, generator: RandomGenerator = Depends(Provide[Container.generator])):
     random_list = generator.generate(n)
     return {"result:": random_list}
