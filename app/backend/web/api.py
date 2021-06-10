58db8244736d2fa0ce1d3348389f24a361522fba
Ruslan <bannikov.ru@inbox.ru>
2021-06-10 21:04:47 +0500

    запись в БД реализована

--- app/backend/web/api.py
+++ app/backend/web/api.py
@@ -14,18 +14,27 @@ router = APIRouter()
 def calculate_plus(x: float, y: float,
                    calculator: Calculator = Depends(Provide[CalcContainer.calculator]),
                    db_repository: DBRepository = Depends(Provide[DBContainer.db_repository])):
+
     db_repository.add_operation_in_time(operation="сложение")
     return {"x": x, "y": y, "result:": calculator.plus(x, y)}
 
 
 @router.put("/calculator/minus")
 @inject
-def calculate_minus(x: float, y: float, calculator: Calculator = Depends(Provide[CalcContainer.calculator])):
+def calculate_minus(x: float, y: float,
+                    calculator: Calculator = Depends(Provide[CalcContainer.calculator]),
+                    db_repository: DBRepository = Depends(Provide[DBContainer.db_repository])):
+
+    db_repository.add_operation_in_time(operation="вычитание")
     return {"x": x, "y": y, "result:": calculator.minus(x, y)}
 
 
 @router.put("/random/range")
 @inject
-def random_range(n: int, generator: RandomGenerator = Depends(Provide[Container.generator])):
+def random_range(n: int,
+                 generator: RandomGenerator = Depends(Provide[Container.generator]),
+                 db_repository: DBRepository = Depends(Provide[DBContainer.db_repository])):
+
+    db_repository.add_operation_in_time(operation="генерация случайных чисел")
     random_list = generator.generate(n)
     return {"result:": random_list}
