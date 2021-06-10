e70fa0524410ee154fafc442404323bd3281cc36
Ruslan <bannikov.ru@inbox.ru>
2021-06-10 20:23:30 +0500

    w

--- app/backend/db/repository_db.py
+++ app/backend/db/repository_db.py
@@ -6,19 +6,20 @@ class DBRepository:
 
     def __init__(self, session_factory):
         self.session_factory = session_factory
+        self.session = session_factory()
 
     def addOperationType(self, operation_type: str) -> None:
-        self.session_factory.add(OperationTypes(operationtype=operation_type))
-        self.session_factory.commit()
+        self.session.add(OperationTypes(operationtype=operation_type))
+        self.session.commit()
 
     def addSingleData(self, operation: str) -> None:
         id_operation_type = self.getOperationTypeID(operation_type=operation)
         now = datetime.now()
         new_operation = OperationsInTime(operation=id_operation_type, time=now)
-        self.session_factory.add(new_operation)
-        self.session_factory.commit()
+        self.session.add(new_operation)
+        self.session.commit()
 
     def getOperationTypeID(self, operation_type: str) -> int:
-        query = self.session_factory.query(OperationTypes).filter(OperationTypes.operationtype == operation_type).first()
+        query = self.session.query(OperationTypes).filter(OperationTypes.operationtype == operation_type).first()
         return query.id_operationtype
 
